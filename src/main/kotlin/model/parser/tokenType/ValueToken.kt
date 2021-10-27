ada982299ca89205684bd1bc38e5abb3e85338f3
Tristan <tristan.boulesteix@gmail.com>
2021-10-27 10:09:04 +0200

    Update ValueToken.kt

--- src/main/kotlin/model/parser/tokenType/ValueToken.kt
+++ src/main/kotlin/model/parser/tokenType/ValueToken.kt
@@ -2,8 +2,8 @@ package fr.tb_lab.model.parser.tokenType
 
 sealed class ValueToken<T>(value: T) : Token<T>(value)
 
-class Value(content: Double) : ValueToken<Double>(content) {
-    constructor(content: String) : this(content.toDouble())
+class Value(value: Double) : ValueToken<Double>(value) {
+    constructor(value: String) : this(value.toDouble())
 }
 
 object InvalidValue : ValueToken<Nothing?>(null)
