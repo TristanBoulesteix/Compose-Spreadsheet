67d918c697beea2eb758baaf747e1b6f7deec700
Tristan <tristan.boulesteix@gmail.com>
2021-11-01 14:59:27 +0100

    Improve comments and allow lowercase for cell reference

--- src/main/kotlin/model/Grid.kt
+++ src/main/kotlin/model/Grid.kt
@@ -3,11 +3,19 @@ package fr.tb_lab.model
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
+/**
+ * Representation of a grid. It is a 2-dimensional list of [Cell]s. There is a private constructor for the serialization
+ * only and a public one for regular usage that take a size of the grid as argument
+ */
 @Serializable
-class Grid(private val list: List<List<Cell>>) : List<List<Cell>> by list {
+class Grid private constructor(private val list: List<List<Cell>>) : List<List<Cell>> by list {
     @Transient
     val alphaHeader = AlphabeticalHeaderList(size)
 
+    /**
+     * Representation of a grid. It is a 2-dimensional list of [Cell]s.
+     * @param gridSize The size of the grid
+     */
     constructor(gridSize: Int) : this(List(gridSize) { List(gridSize) { Cell() } })
 
     fun getCellFromStringCoordinates(coordinates: String): Result<Cell> {
@@ -15,11 +23,11 @@ class Grid(private val list: List<List<Cell>>) : List<List<Cell>> by list {
             val splitId = coordinates.indexOfFirst(Char::isDigit)
             val columnId = coordinates.substring(0 until splitId)
             val rowId = coordinates.substring(splitId until coordinates.length)
-            Result.success(this[rowId.toInt() - 1][alphaHeader.indexOf(columnId)])
+            Result.success(this[rowId.toInt() - 1][alphaHeader.indexOf(columnId.uppercase())])
         } else Result.failure(IllegalArgumentException("Invalid coordinate"))
     }
 
     companion object {
-        val matchCellCoordinate = "^[A-Z]+[1-9]+\$".toRegex()
+        val matchCellCoordinate = "^[A-Za-z]+[1-9]+\$".toRegex()
     }
 }
--- src/main/kotlin/model/parser/Scanner.kt
+++ src/main/kotlin/model/parser/Scanner.kt
@@ -17,30 +17,34 @@ fun scan(expression: String): TokenizedExpression {
     return buildList {
         // We iterate through each character to convert them into Token
         expression.trim().forEachIndexed character@{ index, char ->
-            if (char.isLetter() && (stringBuilder.isEmpty() || stringBuilder.matches("^[A-Z]+\$".toRegex()))) {
-                // if
+            if (char.isLetter() && (stringBuilder.isEmpty() || stringBuilder.matches("^[A-Za-z]+\$".toRegex()))) {
+                // if the char can be part of a cell name, we add it to the StringBuilder and then continue the loop
                 stringBuilder.append(char)
                 return@character
             }
 
+            // We convert the current char to its corresponding token
             val token = if (!char.isWhitespace()) Token.getTokenFromSymbol(char) else return@character
 
             if (token !is Value) {
                 if (index == 0) {
                     when (token) {
+                        // If the first token is a plus, minus or parleft symbol we treat them as unary operator
                         is Add, is Sub -> {
                             stringBuilder.append(char)
                         }
                         is ParLeft -> add(ParLeft)
-                        else -> return listOf(InvalidValue)
+                        else -> return listOf(InvalidValue) // If the symbol is none of the ones cited above, the expression is a syntax error
                     }
                 } else {
                     when {
                         stringBuilder.isNotBlank() -> when {
+                            // If the expression looks like "-(4+1)" we need to convert it to "-1 * (4+1)" to allow the parser to translate it easily
                             token is ParLeft && stringBuilder.last { !it.isWhitespace() } == '-' -> {
                                 add(Value(-1.0))
                                 add(Mul)
                             }
+                            // 2(6+2) is like 2*(6+2)
                             token is ParLeft -> {
                                 add(valueOf(stringBuilder.toString()))
                                 stringBuilder.clear()
@@ -48,26 +52,32 @@ fun scan(expression: String): TokenizedExpression {
                                 add(token)
                                 return@character
                             }
+                            // Dot is a part of a value even though it's not a number
                             token is Dot -> {
                                 stringBuilder.append(char)
                                 return@character
                             }
+                            // Everything else is a normal value. We create a value token from the stringbuilder
                             else -> add(valueOf(stringBuilder.toString()))
                         }
+                        // If the first token after an opening parenthesis is a minus symbol, we treat this operator as unary
                         token is Sub && last() is ParLeft -> {
                             stringBuilder.append(char)
                             return@character
                         }
+                        // (2+4)(6+2) is like (2+4)*(6+2)
                         token is ParLeft && isNotEmpty() && last() is ParRight -> add(Mul)
+                        // A random dot is an error
                         token is Dot -> return listOf(InvalidValue)
                     }
 
                     add(token)
                     stringBuilder.clear()
                 }
-            } else stringBuilder.append(char)
+            } else stringBuilder.append(char) // If the token is a regular value, we add it to the stringbuilder to create a complete number when the next token is an arithmetic symbol
         }
 
+        // We add the last value by converting the stringbuilder to a valueToken
         if (stringBuilder.isNotEmpty()) {
             add(valueOf(stringBuilder.toString()))
         }
